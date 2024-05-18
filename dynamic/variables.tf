ef54d14d51a27a792924efc12fc14c7d333a7285
karunakr-devops <karunakar.etikala@gmail.com>
2024-05-18 13:27:49 +0530

    dynamic

---
+++ dynamic/variables.tf
@@ -0,0 +1,31 @@
+variable "inbound_rules" {
+    type = list
+  
+    default = [
+    {
+        port = 22
+        protocol = "tcp"
+        cidr = ["0.0.0.0/0"]
+    },
+
+    {
+         port = 80
+        protocol = "tcp"
+        cidr = ["0.0.0.0/0"]
+
+    },
+  
+    {
+        port = 3306
+        protocol = "tcp"
+        cidr = ["0.0.0.0/0"]
+    },
+    {
+
+        port = 8080
+        protocol = "tcp"
+        cidr = ["0.0.0.0/0"]
+  }
+    ]
+  
+}
